72492a4d7384169cff2f823c3f1e66d2d1846dd8
Sentio101 <sdr101432@gmail.com>
2022-10-09 14:36:11 -0300

    s

--- src/project.js
+++ src/project.js
@@ -2,7 +2,7 @@
     This JS file will be used for our Mi'kmaq Pictionary Game (Wowkwiss Says).
 
     Authors: 
-    Sebastian Duque Rivera (A00441528)
+    Sebastian Duque Rivera (A00441528) or SDR
     Felipe Duque Rivera ()
     Coco ()
     Adam ()
@@ -14,3 +14,17 @@
  *  This will be used to setp up the main elements of the intro page as soon as you launch it.
  */
 function setup() {} // end setup
+
+/**
+ *  This will record new audio files from the user.
+ */
+function record(
+
+)
+
+/**
+ *  This will close the application.
+ */
+function exit() {
+    window.close();
+}
