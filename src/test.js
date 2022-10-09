c4e094060ebd94cd28daf5306169178ce71e6817
Sentio101 <sdr101432@gmail.com>
2022-10-09 11:18:25 -0300

    Terry's Template

--- src/test.js
+++ src/test.js
@@ -1,2 +1,16 @@
-// This will be used to setp up the main elements of the primary page as soon as you launch it.
+/*
+    This JS file will be used for our Mi'kmaq Pictionary Game (Wowkwiss Says).
+
+    Authors: 
+    Sebastian Duque Rivera (A00441528)
+    Felipe Duque Rivera ()
+    Coco ()
+    Adam ()
+    Mahmood ()
+
+*/
+
+/**
+ *  This will be used to setp up the main elements of the intro page as soon as you launch it.
+ */
 function setup() {} // end setup
